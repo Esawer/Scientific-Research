de9bd4c3a193e6eb05b834bac76abcb02b6b29b8
Esawer <igorewease@gmail.com>
2026-04-21 17:32:16 +0200

    captcha generator features - gauss, salt&pepper, rand lines and char rotation

--- AI & Computer Vision in CAPTCHA Verification/captcha_generator.py
+++ AI & Computer Vision in CAPTCHA Verification/captcha_generator.py
@@ -1,8 +1,11 @@
 import random
+from pathlib import Path
 from PIL import Image, ImageDraw, ImageFilter, ImageFont, ImageColor
 
+img_dir = Path.cwd() / 'AI & Computer Vision in CAPTCHA Verification' / 'captcha_images'
+
 def string_generator():
-    char_condition =  "\\/ilIo0O.,:`_-=*()[}><|+!]{\"\'"
+    char_condition =  "\\/ilIjo0O.,:;`_^-=*()[}><|+!]{\"\'"
     str_len = random.randint(5,8)
     str_final = ""
 
@@ -18,34 +21,55 @@ def string_generator():
 
 
 def captcha_generator(number_of_images: int):
-    captcha_fonts = ['arialbd.ttf','arialbi.ttf']
-    img_width = 320
+    captcha_fonts = ['arialbd.ttf', 'arialbi.ttf', 
+                     'timesbd.ttf', 'timesbi.ttf', 
+                     'segoeuib.ttf', 'segoeuiz.ttf', 
+                     'robotob.ttf', 'robotobi.ttf']
+    img_width = 340
     img_height = 110
 
     for i in range(number_of_images):
+        char_counter = 0
         str_code = string_generator()
         bg_color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+        luminace = (0.2126*bg_color[0] + 0.7152*bg_color[1] + 0.0722*bg_color[2]) / 255
+
+        text_color = (random.randint(0,70), random.randint(0,70), random.randint(0,70)) if luminace > 0.5 \
+                else (random.randint(180,255), random.randint(180,255), random.randint(180,255))
         
         im = Image.new(size=(img_width,img_height), mode="RGB", color=bg_color)
+        im_draw = ImageDraw.Draw(im)
 
         for j in str_code:
-            img_char = Image.new(size=(50,50), mode="RGBA", color=(0,0,0,0))
+            img_char = Image.new(size=(50,50), mode="RGBA")
             img_char_text = ImageDraw.Draw(img_char)
             img_font = ImageFont.truetype(captcha_fonts[random.randint(0,1)], size=random.randint(35,40))
-            img_char_text.text((5,5), j, font=img_font)    
-            img_char.rotate(random.randint(-55,-50))
+            img_char_text.text((5,5), j, font=img_font, fill=text_color)    
+            img_char = img_char.rotate(random.randint(-55, 55))
 
-            im.paste(img_char, ((45*str_code.index(j)),random.randint(0,60)), mask=img_char)
-        im.save(f"{"".join(str_code)}_{i}.png")
+            im.paste(img_char, ((42*char_counter),random.randint(0,60)), mask=img_char)
+            char_counter += 1
 
-"""
-fix: not rotation for chars;
-fix: same char on top of the same char - if both the same
-add: random spacing 40-50 between paste
+        pixels = list(im.getdata()) # type: ignore
+        
+        for pixel in pixels:
+            ...
 
-
-"""
+        """for line in range(random.randint(4,8)):
+            im_draw.line(([random.randint(0, img_width)-50, random.randint(0, img_height)-50],
+                          [random.randint(0, img_width)+50, random.randint(0, img_height)+50]), 
+                          fill=text_color, width=random.randint(2,3))
             
+        for salt_and_pepper in range(12000):
+            im_draw.point([random.randint(0, img_width), random.randint(0, img_height)], fill=text_color)
+        
+        im_gaussian = ImageFilter.GaussianBlur(radius=random.uniform(1.2, 1.8))
+        im =im.filter(im_gaussian)"""
+        str_code = "".join(str_code).replace("?","0")
+
+        im.save(fr"{img_dir}\\{i}.{str_code}.png")
+
+  
 
 
 
