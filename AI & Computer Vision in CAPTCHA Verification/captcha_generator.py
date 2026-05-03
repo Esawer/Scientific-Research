9e5d21e611357f91e9c6f609cc925b8cd4289215
Esawer <igorewease@gmail.com>
2026-05-03 19:04:54 +0200

    feat(AI & Computer Vision in CAPTCHA Verification): Added an open source CAPTCHA, included basic sqllite database integration.

--- AI & Computer Vision in CAPTCHA Verification/captcha_generator.py
+++ AI & Computer Vision in CAPTCHA Verification/captcha_generator.py
@@ -2,12 +2,15 @@ import random
 import math
 import numpy as np
 from pathlib import Path
-from PIL import Image, ImageDraw, ImageFilter, ImageFont, ImageColor
+from PIL import Image, ImageDraw, ImageFilter, ImageFont
+from captcha.image import ImageCaptcha
+import captcha_db
 
-img_dir = Path.cwd() / 'AI & Computer Vision in CAPTCHA Verification' / 'captcha_images'
+
+img_dir = Path.cwd() / 'AI & Computer Vision in CAPTCHA Verification'
 
 def string_generator():
-    char_condition =  "\\/ilIjo0O.,:;`_^-=*()[}><|+!]{\"\'"
+    char_condition =  "\\/ilIjyo10Oxsvw.,:;`_^-=*()[}><|+!]{\"\'"
     str_len = random.randint(5,8)
     str_final = ""
 
@@ -25,13 +28,12 @@ def string_generator():
 def captcha_generator(number_of_images: int):
     captcha_fonts = ['arialbd.ttf', 'arialbi.ttf', 
                      'timesbd.ttf', 'timesbi.ttf', 
-                     'segoeuib.ttf', 'segoeuiz.ttf', 
-                     'robotob.ttf', 'robotobi.ttf']
-    img_width = 340
-    img_height = 110
+                     'segoeuib.ttf']
+    img_width = 400
+    img_height = 150
 
     for i in range(number_of_images):
-        char_counter = 1
+        char_counter = 0.1
         str_code = string_generator()
         bg_color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
         luminace = (0.2126*bg_color[0] + 0.7152*bg_color[1] + 0.0722*bg_color[2]) / 255
@@ -42,19 +44,19 @@ def captcha_generator(number_of_images: int):
         im = Image.new(size=(img_width,img_height), mode="RGB", color=bg_color)
 
         for j in str_code:
-            img_char = Image.new(size=(50,50), mode="RGBA")
+            img_char = Image.new(size=(80,80), mode="RGBA")
             img_char_text = ImageDraw.Draw(img_char)
-            img_font = ImageFont.truetype(captcha_fonts[random.randint(0,1)], size=random.randint(35,40))
+            img_font = ImageFont.truetype(captcha_fonts[random.randint(0,4)], size=random.randint(55,65))
             img_char_text.text((5,5), j, font=img_font, fill=text_color)    
-            img_char = img_char.rotate(random.randint(-55, 55))
+            img_char = img_char.rotate(random.randint(-40, 40))
 
-            im.paste(img_char, ((36*char_counter),random.randint(0,60)), mask=img_char)
+            im.paste(img_char, ((int(50*char_counter)),random.randint(0,60)), mask=img_char)
             char_counter += 1
 
-        pixels = im.load() # type: ignore
+        pixels = im.load() 
         new_img = np.array([[[0,0,0]]*(img_width)]*(img_height))
         new_img = new_img.astype(np.uint8)
-        rand_amplitude = random.uniform(5,7)
+        rand_amplitude = random.uniform(4,6)
 
         for j in range(img_width):
             for k in range(img_height):
@@ -63,25 +65,39 @@ def captcha_generator(number_of_images: int):
 
         im = Image.fromarray(new_img)
         im_draw = ImageDraw.Draw(im)
-
-                
-        for line in range(random.randint(4,8)):
+         
+        for line in range(random.randint(6,8)):
             im_draw.line(([random.randint(0, img_width)-50, random.randint(0, img_height)-50],
                           [random.randint(0, img_width)+50, random.randint(0, img_height)+50]), 
                           fill=text_color, width=random.randint(2,3))
             
-        for salt_and_pepper in range(12000):
+        for salt_and_pepper in range(30000):
             im_draw.point([random.randint(0, img_width), random.randint(0, img_height)], fill=text_color)
         
-        im_gaussian = ImageFilter.GaussianBlur(radius=random.uniform(1.2, 1.8))
+        im_gaussian = ImageFilter.GaussianBlur(radius=random.uniform(1.2, 1.4))
         im = im.filter(im_gaussian)
-        str_code = "".join(str_code).replace("?","0")
+        str_code = "".join(str_code)
 
-        im.save(fr"{img_dir}\\{i}.{str_code}.png")
+        image = ImageCaptcha(fonts=['arialbd.ttf', 'arialbi.ttf', 
+                                    'timesbd.ttf', 'timesbi.ttf', 
+                                    'segoeuib.ttf'])
+        image.character_rotate = (-40, 40)
+        image.character_offset_dx
+        image.write(str_code, f'{img_dir}\\opensrc_captcha_images\\os{i}.png')
+        
+        image_resize = Image.open(f"{img_dir}\\opensrc_captcha_images\\os{i}.png")
+        image_resize = image_resize.resize((int(image_resize.width * 2.5), int(image_resize.height * 2.5)))
+        image_resize.save(f"{img_dir}\\opensrc_captcha_images\\os{i}.png")
+        with open (f"{img_dir}\\opensrc_captcha_images\\os{i}.png", 'rb') as f:
+            blob = f.read()
+            captcha_db.add_item(table_name="opensource_implementation", file_name=f"os{i}.png", captcha_code=str_code, captcha_image=blob) # type: ignore
 
-  
+        im.save(f"{img_dir}\\author_captcha_images\\a{i}.png")
+        with open (f"{img_dir}\\author_captcha_images\\a{i}.png", 'rb') as f:
+            blob = f.read()
+            captcha_db.add_item(table_name="author_implementation", file_name=f"a{i}.png", captcha_code=str_code, captcha_image=blob) # type: ignore
 
 
-
-  
-print(captcha_generator(1))
+captcha_db.delte_all(table_name="opensource_implementation") # type: ignore
+captcha_db.delte_all(table_name="author_implementation") # type: ignore
+captcha_generator(10)
